73ff390c72a37ec0e58b5acd6d9fe923aa354471
erickdelcastillordz98-sudo <erickdelcastillordz98@gmail.com>
2026-03-26 06:49:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,24 +2,42 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-
 const API = process.env.API_URL;
 
+// MENÚ PRINCIPAL
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, '💸 Bienvenido a ServyPayAccess');
-});
-
-bot.onText(/\/pago (.+)/, async (msg, match) => {
-  const monto = parseFloat(match[1]);
-
-  const res = await axios.post(`${API}/pago`, {
-    monto
+  bot.sendMessage(msg.chat.id, '💸 Bienvenido a ServyPayAccess', {
+    reply_markup: {
+      keyboard: [
+        ['💰 Crear pago'],
+        ['📊 Ver saldo']
+      ],
+      resize_keyboard: true
+    }
   });
-
-  bot.sendMessage(msg.chat.id, `✅ Pago simulado\n💰 Saldo: ${res.data.saldo}`);
 });
 
-bot.onText(/\/saldo/, async (msg) => {
-  const res = await axios.get(`${API}/saldo`);
-  bot.sendMessage(msg.chat.id, `💰 Saldo actual: ${res.data.saldo}`);
+// RESPUESTAS
+bot.on('message', async (msg) => {
+  const text = msg.text;
+
+  if (text === '💰 Crear pago') {
+    bot.sendMessage(msg.chat.id, 'Escribe el monto así:\nEjemplo: 100');
+  }
+
+  else if (text === '📊 Ver saldo') {
+    const res = await axios.get(`${API}/saldo`);
+    bot.sendMessage(msg.chat.id, `💰 Saldo actual: ${res.data.saldo}`);
+  }
+
+  // SI ES NÚMERO (CREA PAGO)
+  else if (!isNaN(text)) {
+    const monto = parseFloat(text);
+
+    const res = await axios.post(`${API}/pago`, {
+      monto
+    });
+
+    bot.sendMessage(msg.chat.id, `✅ Pago creado\n💰 Nuevo saldo: ${res.data.saldo}`);
+  }
 });
